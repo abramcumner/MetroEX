6de31d620825f012f5cc04593d2008de801c6f24
slik <slik.jay@gmail.com>
2019-04-09 00:23:37 +0300

    * updated texture properties viewer

--- src/ui/TexturePropertiesViewer.h
+++ src/ui/TexturePropertiesViewer.h
@@ -32,14 +32,53 @@ namespace MetroEX {
             String^ get() { return marshal_as<String^>(this->mTextureInfo->name); }
         };
 
-        property uint8_t Flags {
-            uint8_t get() { return this->mTextureInfo->flags; }
+        [Category("Common")]
+        [Description("Texture flags")]
+        property String^ Flags {
+            String^ get() { return Convert::ToString(this->mTextureInfo->flags, 2)->PadLeft(8, L'0'); }
         }
 
-        property uint32_t Type {
-            uint32_t get() { return this->mTextureInfo->type; }
+        [Category("Common")]
+        [Description("Texture type")]
+        property String^ Type {
+            String^ get() {
+                switch (safe_cast<MetroTextureInfo::TextureType>(this->mTextureInfo->type)) {
+                case MetroTextureInfo::TextureType::Diffuse:
+                    return "Diffuse";
+                case MetroTextureInfo::TextureType::Detail_diffuse:
+                    return "Detail_diffuse";
+                case MetroTextureInfo::TextureType::Cubemap:
+                    return "Cubemap";
+                case MetroTextureInfo::TextureType::Cubemap_hdr:
+                    return "Cubemap_hdr";
+                case MetroTextureInfo::TextureType::Terrain:
+                    return "Terrain";
+                case MetroTextureInfo::TextureType::Bumpmap:
+                    return "Bumpmap";
+                case MetroTextureInfo::TextureType::Diffuse_va:
+                    return "Diffuse_va";
+                case MetroTextureInfo::TextureType::Arbitrary4:
+                    return "Arbitrary4";
+                case MetroTextureInfo::TextureType::Normalmap:
+                    return "Normalmap";
+                case MetroTextureInfo::TextureType::Normalmap_alpha:
+                    return "Normalmap_alpha";
+                case MetroTextureInfo::TextureType::Normalmap_detail:
+                    return "Normalmap_detail";
+                case MetroTextureInfo::TextureType::Unknown_01:
+                    return "Unknown_01";
+                case MetroTextureInfo::TextureType::Unknown_has_lum:
+                    return "Unknown_has_lum";
+                case MetroTextureInfo::TextureType::Instance:
+                    return "Instance";
+                default:
+                    return "Unknown";
+                }
+            }
         }
 
+        [Category("Common")]
+        [Description("Texture source name")]
         property String^ SourceName {
             String^ get() {
                 if (mTextureInfo->source_name.IsValidRef()) {
@@ -58,10 +97,14 @@ namespace MetroEX {
             uint32_t get() { return this->mTextureInfo->format; }
         }
 
+        [Category("Size")]
+        [Description("Texture Width in pixels")]
         property uint32_t Width {
             uint32_t get() { return this->mTextureInfo->width; }
         }
 
+        [Category("Size")]
+        [Description("Texture Height in pixels")]
         property uint32_t Height {
             uint32_t get() { return this->mTextureInfo->height; }
         };
@@ -70,6 +113,8 @@ namespace MetroEX {
             bool get() { return this->mTextureInfo->animated; }
         };
 
+        [Category("Common")]
+        [Description("Texture real path")]
         property String^ RealPath {
             String^ get() { return this->mRealPath; }
         };
